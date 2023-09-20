332814c6ff1dbbe9671f78baef8393b918bc7987
ayoubleass <ayoubleas@gmail.com>
2023-09-20 01:46:38 -0700

    file : _printf.c

--- _printf.c
+++ _printf.c
@@ -1,64 +1,24 @@
 #include "main.h"
 
-/**
- * _printf - Custom implementation of printf function ,
- * with limited format specifiers.
- * @format: The format string containing format specifiers.
- * Return: The number of characters printed.
- */
 
 
-
-
-int handleSpecifier(char c , va_list args);
-
-int _printf(const char *format, ...)
-{
-	va_list args;
-
-	int i = 0;
-	int size = 0;
-
-	if (!format || (format[0] == '%' && format[1] == '\0')) 
-		return (-1);
-
-	va_start(args, format);
-  
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			size++;
-		}
-    
-		else if(format[i] == '%'){
-			size += handleSpecifier(format[i + 1], args);  
-			i++;
-		}
-	i++;
-	}
-    
-	va_end(args);
-	return (size);
-}
 
 
 /**
  * handleSpecifier - Handle the following conversion specifiers: s,c,i,d...
- *@c: specifier
- *@args: variadic arguments
- *Return: the length of the argument
+ * @c: specifier
+ * @args: variadic arguments
+ * Return: the length of the argument
  */
 
 
 int handleSpecifier(char c, va_list args)
 {
 	int size = 0;
-	
-	switch(c)
+
+	switch (c)
 	{
-		case 'c' :
+		case 'c':
 		{
 			_putchar(va_arg(args, int));
 			size++;
@@ -69,8 +29,8 @@ int handleSpecifier(char c, va_list args)
 			size = printString(va_arg(args, char *));
 			break;
 		}
-		case  'd': 
-		case  'i': 
+		case  'd':
+		case  'i':
 		{
 			size = print_integer(va_arg(args, int));
 			break;
@@ -80,3 +40,41 @@ int handleSpecifier(char c, va_list args)
 }
 
 
+
+/**
+ * _printf - Custom implementation of printf function ,
+ * with limited format specifiers.
+ * @format: The format string containing format specifiers.
+ * Return: The number of characters printed.
+ */
+
+
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	int i = 0;
+	int size = 0;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			size++;
+		}
+		else if (format[i] == '%')
+		{
+			size += handleSpecifier(format[i + 1], args);
+			i++;
+		}
+	i++;
+	}
+	va_end(args);
+	return (size);
+}
